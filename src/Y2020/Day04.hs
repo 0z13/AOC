9933e727f9377ebad91f6f06a8b7ef2edb86d8c9
Joe <zielinski@fastmail.com>
2021-05-12 08:56:05 +0200

    n

--- src/Y2020/Day04.hs
+++ src/Y2020/Day04.hs
@@ -3,18 +3,12 @@
 module Y2020.Day04 where
 
 import Lib
+import Data.List.Split
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 main4 :: IO ()
 main4 = do
-  xs <- go . T.splitOn "\n"  <$> TIO.readFile "../data_2020/day04.txt"
-  print $ head . tail $ fmap T.unpack xs
+  xs <-  map (\. splitOn " " . unlines . lines <$> readFile "../data_2020/day04.txt"
+  print xs 
 
-go :: [T.Text] -> [T.Text] 
-go = f [] [] 
 
-f :: [T.Text] -> [T.Text] -> [T.Text] -> [T.Text]
-f res   _    []  =  res
-f res acc (x:xs) 
-  | x == "" = f (acc ++ res) [] xs
-  | otherwise = f res (x : acc) xs
