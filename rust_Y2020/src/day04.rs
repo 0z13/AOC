e74352a3a64618229266d3042c9b77a556ec43ba
Joe <zielinski@fastmail.com>
2021-05-29 22:01:18 +0200

    zz

--- rust_Y2020/src/day04.rs
+++ rust_Y2020/src/day04.rs
@@ -1,8 +1,20 @@
 //j
 //
-
 pub fn solve() {
-
-    println!("what")
+    let xs =  include_str!("./data/day04.txt").lines();
+    let mut tmp: Vec<String> = Vec::new();
+    let mut data: Vec<String> = Vec::new();
+    for lines in xs {
+        if lines == "" {
+            println!("nweline")
+        } else {
+            tmp.push(lines.to_string());
+            println!("nothing here bubba");
+        }
+        
+    }
 
 }
+
+
+
